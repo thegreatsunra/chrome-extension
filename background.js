9e94604d6761c92b7c66c5dd25fb3ce4eca8489f
Dane Petersen <thegreatsunra@gmail.com>
2016-10-24 21:10:05 -0500

    Update example to insert content into the DOM

--- background.js
+++ background.js
@@ -1,1 +1,11 @@
-document.body.style.backgroundColor="red";
+'use strict';
+
+var target = document.getElementsByClassName('c-content');
+var content = document.createElement('h1');
+var text = document.createTextNode('This content was injected by a chrome extension!');
+content.appendChild(text);
+
+for (var i = target.length - 1; i >= 0; i--) {
+  console.log('tried adding a thing');
+  target[i].appendChild(content);
+}
