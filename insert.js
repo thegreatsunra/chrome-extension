38e6f1dbed30d1cafa4e4fc91783db91b5499470
Dane Petersen <thegreatsunra@gmail.com>
2016-10-24 21:36:29 -0500

    Clarify variable names and insert content into portfolio

--- insert.js
+++ insert.js
@@ -1,11 +1,15 @@
 'use strict';
 
-var target = document.getElementsByClassName('c-content');
-var content = document.createElement('h1');
-var text = document.createTextNode('This content was injected by a chrome extension!');
-content.appendChild(text);
+// var parent = document.getElementById('parent');
+// var existingThing = document.getElementById('existing-thing');
 
-for (var i = target.length - 1; i >= 0; i--) {
-  console.log('tried adding a thing');
-  target[i].appendChild(content);
-}
+var parent = document.getElementsByClassName('c-content');
+var parent = parent[0];
+var existingThing = parent.getElementsByClassName('o-layout');
+var existingThing = existingThing[0];
+
+var newThing = document.createElement('h1');
+var text = document.createTextNode('This content was injected by a chrome extension!');
+newThing.appendChild(text);
+
+parent.insertBefore(newThing, existingThing);
