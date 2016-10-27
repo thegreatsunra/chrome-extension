0a8f7f0ca13cfc25a58931efad92dc5a80c1adda
Dane Petersen <thegreatsunra@gmail.com>
2016-10-26 20:30:19 -0500

    Tweak class names and add more carousel markup

--- insert.js
+++ insert.js
@@ -3,7 +3,7 @@
 function insertElement(parentObject, elementObject) {
   var newElement = document.createElement(elementObject.tag);
   // var text = document.createTextNode(elementObject.text);
-  var markup = '<div class="c-nav"><a href="#" class="c-nav-link c-nav-link--previous"><i class="c-icon c-icon-chevron c-icon-chevron--previous"></i></a><a href="#" class="c-nav-link c-nav-link--next"><i class="c-icon c-icon-chevron c-icon-chevron--next"></i></a></div><div class="c-content">' + elementObject.text + '</div>';
+  var markup = '<div class="c-carousel__controls"><a href="#" class="c-carousel__control-link c-carousel__control-link--previous"><i class="c-carousel__control-icon c-carousel__control-icon-chevron c-carousel__control-icon-chevron--previous"></i></a><a href="#" class="c-carousel__control-link c-carousel__control-link--next"><i class="c-carousel__control-icon c-carousel__control-icon-chevron c-carousel__control-icon-chevron--next"></i></a></div><div class="c-carousel"><div class="c-carousel__viewport"><div class="c-carousel__card">' + elementObject.text + '</div><div class="c-carousel__card"><div class="c-carousel__card">Another thing with content!</div></div></div>';
   // grab the element with that id
   if (parentObject.id) {
     var parent = document.getElementById(parentObject.id);
@@ -53,8 +53,8 @@ var parentElement = {
 
 // id, class, tag
 var childElement = {
-  id: 'id-new-element',
-  class: 'c-new-element',
+  id: 'id-socialator',
+  class: 'c-socialator',
   tag: 'div',
   text: 'This content was injected by a chrome extension!',
 }
