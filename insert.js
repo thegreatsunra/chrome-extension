a6b41b5f05a2331fc55016dc4d862e5ba6cb9a87
Dane Petersen <thegreatsunra@gmail.com>
2016-10-26 22:42:20 -0500

    Add disabled classes and targetable ids to controls

--- insert.js
+++ insert.js
@@ -3,7 +3,7 @@
 function insertElement(parentObject, elementObject) {
   var newElement = document.createElement(elementObject.tag);
   // var text = document.createTextNode(elementObject.text);
-  var markup = '<div class="c-carousel__controls"><a href="#" class="c-carousel__control-link c-carousel__control-link--previous"><i class="c-carousel__control-icon c-carousel__control-icon-chevron c-carousel__control-icon-chevron--previous"></i></a><a href="#" class="c-carousel__control-link c-carousel__control-link--next"><i class="c-carousel__control-icon c-carousel__control-icon-chevron c-carousel__control-icon-chevron--next"></i></a></div><div class="c-carousel__viewport"><div class="c-carousel"><div class="c-carousel__card">' + elementObject.text + '</div><div class="c-carousel__card">Another thing with content!</div></div></div>';
+  var markup = '<div class="c-carousel__controls"><a href="#" class="c-carousel__control-link c-carousel__control-link--previous c-carousel__control-link--disabled" id="id-carousel-control-link-previous"><i class="c-carousel__control-icon c-carousel__control-icon-chevron c-carousel__control-icon-chevron--previous"></i></a><a href="#" class="c-carousel__control-link c-carousel__control-link--next" id="id-carousel-control-link-next"><i class="c-carousel__control-icon c-carousel__control-icon-chevron c-carousel__control-icon-chevron--next"></i></a></div><div class="c-carousel__viewport"><div class="c-carousel" id="id-carousel"><div class="c-carousel__card">' + elementObject.text + '</div><div class="c-carousel__card">Another thing with content!</div></div></div>';
   // grab the element with that id
   if (parentObject.id) {
     var parent = document.getElementById(parentObject.id);
